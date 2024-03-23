013f82fb05f28d1049a64719584e55f93b6516ec
BeyondXinXin <779354187@qq.com>
2024-03-23 14:39:07 +0000

    Update PicX 90fe2abb005242c000a1e7491c7807f11df78016

--- pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.d9bd85f9.js
+++ pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.d9bd85f9.js
@@ -16,5 +16,5 @@
 
   window.__checkUpdateSetup__({"logVersion":true,"injectFileBase":"./"});
       ;const logFn = function pt(t,s){console.log(`version: %c${t}`,"color: #1677ff"),console.log(`release time: %c${new Date(s).toLocaleString()}`,"color: #1677ff")}
-      ;logFn('4a31c96', 1711204470259)
+      ;logFn('90fe2ab', 1711204691744)
     
